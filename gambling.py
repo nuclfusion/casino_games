eeb6fb01fd2f52fe125db73142ad451e1790b0a4
nuclfusion <jorge@teamhurtado.com>
2020-01-25 22:23:51 -0500

    Update gambling.py

--- gambling.py
+++ gambling.py
@@ -1,20 +1,28 @@
 import random
+import matplotlib.pyplot as plt
 
 # Roulette betting; colors only
 def roulette(bet, color, spins):
+    x = []
+    y = []
     wheel = ['red']*18 + ['black']*18 + ['green']*2
     winnings = 0
     restore_bet = bet
     for spin in range(0, spins):
-        print(winnings)
         rand = random.choice(wheel)
         if color == rand:
             winnings = winnings + bet
             bet = restore_bet
+            x.append(spin)
+            y.append(winnings)
         else:
             winnings = winnings - bet
             bet = bet*2
+            x.append(spin)
+            y.append(winnings)
+    plt.plot(x,y)
+    plt.show()
     return winnings
 
 
-print(roulette(2, 'red', 100))
+print(roulette(1, 'red', 100))
